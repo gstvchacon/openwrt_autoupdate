e3d33365858a0261ef9332c8384342c3546ded16
Gustavo Chacon <gstvchacon@gmail.com>
2025-05-16 01:28:59 -0300

    add automated reboot for future functions. Reingeniering whole updater code

--- openwrt_autoupdate.sh
+++ openwrt_autoupdate.sh
@@ -1,16 +1,52 @@
 #/usr/bin/env bash
+##gstvchacon@gmail.com
+##Version 2
+##OpenWRT
+set -x
 
-time=$(date +%y-%m-%dT%H:%M:%S)
-dates=$(date +%y-%m-%d)
-echo '=======================================================' >>auto_update_log.log
-echo 'init time for update: '$time >>auto_update_log.log
-opkg update >>auto_update_log.log 2>&1
+function updater {
+	opkg update >> $1
+}
 
-for i in $(opkg list-upgradable|awk '{print $1}') 
-do
-	echo $time $i>>auto_update_log.log
-	opkg upgrade $i >>auto_update_log.log 2>&1	
-done; 
-time=$(date +%y-%m-%dT%H:%M:%S)
-echo 'end time :' $time>>auto_update_log.log
-echo '=======================================================' >>auto_update_log.log
+function message {
+	
+	time=$(date +%y-%m-%d-T%H:%M:%S)
+	dates=$(date +%y%m%d)
+	bar='======================================================='
+	cur_log_fiel="auto_update_$dates.log"
+	int="$time: init time for update."
+	updating="$time: Updating: "
+	none="$time: No updates avalible."
+	error="No valid code message."
+	case $1 in
+		1)
+			echo $bar >> $cur_log_fiel
+			echo $int >> $cur_log_fiel
+			updater $cur_log_fiel
+			;;
+		2)
+			echo $updating $2 >> $cur_log_fiel
+			;;
+		3) 	
+			echo $none >> $cur_log_fiel
+			;;
+		*)
+			echo $error
+			;;
+		esac
+}
+
+
+message 1
+updatable=$(opkg list-upgradable|awk '{print $1}')
+count=$(echo $updatable | wc -w) 
+if [ ! $count -eq 0 ] 
+then
+	for i in  $updatable
+	do
+		message 2 $i	
+	done; 
+else
+	message 3
+fi
+exit
